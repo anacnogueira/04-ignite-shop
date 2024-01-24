3119369495570b0e2a42750e2e8ae07325dc7273
Ana Claudia <anacnogueira@gmail.com>
2024-01-24 15:16:58 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,26 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Home() {
   const { query } = useRouter();
   return (
-    <>
-      <h1>product {JSON.stringify(query)}</h1>
-    </>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+          reprehenderit, inventore enim aliquam expedita aspernatur fugiat
+          nostrum nulla! Accusamus inventore ad ea minima quisquam sequi modi
+          hic eos magnam repellat?
+        </p>
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
