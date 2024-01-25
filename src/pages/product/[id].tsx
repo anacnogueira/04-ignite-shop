05e603552b39a35418898f49321eb35e074f75af
Ana Claudia <anacnogueira@gmail.com>
2024-01-25 19:24:52 -0300

    Falback do SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -39,7 +39,7 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [{ params: { id: "prod_kNNKJKJKJJ" } }],
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
